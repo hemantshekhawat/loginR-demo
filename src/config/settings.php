7a9e105b4df1c2c195c3452f7528ec01eaa624fb
Hemant Shekhawat <hemantshekhawat@users.noreply.github.com>
2016-04-18 19:27:08 +0530

    updated
    removed keys

--- src/config/settings.php
+++ src/config/settings.php
@@ -8,7 +8,7 @@
 
 define("MAILCHIMP_API_KEY","6675ab0be1d89ed9072f2f907c8046e2-us12");
 
-define('LR_APP_NAME', 'lr-ashur1'); // Replace LOGINRADIUS_SITE_NAME_HERE with your site name that provide in LoginRadius account.
-define('LR_API_KEY', '68586157-f6b4-493f-9a7c-bebfe80d82a3'); // Replace LOGINRADIUS_API_KEY_HERE with your site API key that provide in LoginRadius account.
-define('LR_API_SECRET', '1ceef962-5252-415e-b6de-bce9cff7af6a'); // Replace LOGINRADIUS_API_SECRET_HERE with your site Secret key that provide in LoginRadius account.
+define('LR_APP_NAME', 'xx-xxxxxx'); // Replace LOGINRADIUS_SITE_NAME_HERE with your site name that provide in LoginRadius account.
+define('LR_API_KEY', 'xxxxxxxxx-xxxxxxx-xxxxxxxx-xxxxxxxxxxxxxxx'); // Replace LOGINRADIUS_API_KEY_HERE with your site API key that provide in LoginRadius account.
+define('LR_API_SECRET', 'xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxx'); // Replace LOGINRADIUS_API_SECRET_HERE with your site Secret key that provide in LoginRadius account.
 define('PAGEPATH', 'http://localhost/'); //example: http://example.com/
