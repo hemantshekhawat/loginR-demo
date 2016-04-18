f00dc406ddfef52281f62488c0b326f5b1d6f712
Hemant Shekhawat <hemantshekhawat@users.noreply.github.com>
2016-04-18 19:27:45 +0530

    Update settings.php

--- src/config/settings.php
+++ src/config/settings.php
@@ -6,7 +6,7 @@
  * Time: 1:53 PM
  */
 
-define("MAILCHIMP_API_KEY","6675ab0be1d89ed9072f2f907c8046e2-us12");
+define("MAILCHIMP_API_KEY","6675abxxxxxxxxxxxxxxxxxxxxxxxxxxx046e2-us12");
 
 define('LR_APP_NAME', 'xx-xxxxxx'); // Replace LOGINRADIUS_SITE_NAME_HERE with your site name that provide in LoginRadius account.
 define('LR_API_KEY', 'xxxxxxxxx-xxxxxxx-xxxxxxxx-xxxxxxxxxxxxxxx'); // Replace LOGINRADIUS_API_KEY_HERE with your site API key that provide in LoginRadius account.
